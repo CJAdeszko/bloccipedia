7f74d622fe6b70b4c9b7157128e8ec749ea630d4
Christopher Adeszko <cjadeszko@gmail.com>
2018-02-04 19:10:17 -0700

    Commit changes to push to heroku

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,71 @@
+class WikisController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :index]
+
+
+  def index
+    @wikis = Wiki.all
+  end
+
+
+  def new
+    @wiki = Wiki.new
+  end
+
+
+  def create
+    @wiki = Wiki.new(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to [@wiki]
+    else
+      flash.now[:alert] = "There was an error saving the Wiki. Please try again."
+      render :new
+    end
+  end
+
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.assign_attributes(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated."
+      redirect_to [@wiki]
+    else
+      flash.now[:alert] = "There was an error saving the Wiki. Please try again."
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "#{wiki.title} was deleted successfully."
+      redirect_to [@wiki]
+    else
+      flash.now[:alert] = "There was an error deleting the Wiki. Please try again."
+      render :show
+    end
+  end
+
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :wikis
+  
   get 'welcome/index'
 
   get 'welcome/about'
