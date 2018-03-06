5bb4dc00b2ab8dce6faee9d72dc8bd8089ccaf94
Christopher Adeszko <cjadeszko@gmail.com>
2018-03-05 17:45:15 -0700

    Feature - Markdown: complete

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,2 +1,22 @@
 module WikisHelper
+  def markdown
+    render_options = {
+      filter_html: true,
+      no_links: true,
+      hard_wrap: true,
+    }
+
+    extensions = {
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      strikethrough: true,
+      superscript: true,
+      underline: true,
+      highlight: true,
+      quote: true,
+    }
+
+    renderer = Redcarpet::Render::HTML.new(render_options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+  end
 end
